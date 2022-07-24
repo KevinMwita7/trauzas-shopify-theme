cfa4e6c6832bda08e43d336e8470344a29c8e5ea
KevinMwita7 <kevinmwita7@gmail.com>
2022-07-24 17:59:26 +0000

    initialize collection list slide

---
+++ assets/collection-slide.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let swiper = new Swiper('.collection-list-swiper', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        autoplay: true,
+        // Optional parameters
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }
+    });
+
+    if(Shopify.designMode) {
+      document.addEventListener("shopify:section:load", function (event) {
+        swiper = new Swiper('.swiper', options);
+      });
+  
+      document.addEventListener("shopify:section:unload", function (event) {
+          swiper.destroy();
+      });
+          
+      document.addEventListener("shopify:block:select", function(event) {
+        var block = JSON.parse(event.target.dataset.shopifyEditorBlock);
+        if(block.type === "banner_slide") {
+          console.log("Swiper block is", event.target.dataset.slideIndex)
+          swiper.slideTo(+event.target.dataset.slideIndex);
+        }
+      });
+    }
+});
