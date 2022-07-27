78ce48c5bcde88f21e0e0f8f4f8d9be106f3b4d5
KevinMwita7 <kevinmwita7@gmail.com>
2022-07-27 18:19:53 +0000

    apply slides per view mobile settings

--- assets/collection-slide.js
+++ assets/collection-slide.js
@@ -1,30 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-    let swiper = new Swiper('.collection-list-swiper', {
-        slidesPerView: +document.getElementsByClassName("collection-list-swiper")[0].getAttribute("data-slides-per-view"),
-        spaceBetween: 30,
-        // If we need pagination
-        pagination: {
-          el: '.collection-list-swiper-pagination',
-          clickable: true,
-        },
+document.addEventListener('DOMContentLoaded', function () {
+  let swiperHtmlEl = document.getElementById("collection-list-swiper");
+
+  let swiper = new Swiper('.collection-list-swiper', {
+    slidesPerView: screen.width < 750 ? +swiperHtmlEl.getAttribute("data-slides-per-view") : +swiperHtmlEl.getAttribute("data-slides-per-view-desktop"),
+    spaceBetween: 30,
+    // If we need pagination
+    pagination: {
+      el: '.collection-list-swiper-pagination',
+      clickable: true,
+    },
+  });
+
+  if (Shopify.designMode) {
+    document.addEventListener("shopify:section:load", function (event) {
+      swiper = new Swiper('.swiper', options);
     });
 
-    if(Shopify.designMode) {
-      document.addEventListener("shopify:section:load", function (event) {
-        swiper = new Swiper('.swiper', options);
-      });
-  
-      document.addEventListener("shopify:section:unload", function (event) {
-          swiper.destroy();
-      });
-          
-      document.addEventListener("shopify:block:select", function(event) {
-        var block = JSON.parse(event.target.dataset.shopifyEditorBlock);
-        if(block.type === "banner_slide") {
-          console.log("Swiper block is", event.target.dataset.slideIndex)
-          swiper.slideTo(+event.target.dataset.slideIndex);
-        }
-      });
-    }
+    document.addEventListener("shopify:section:unload", function (event) {
+      swiper.destroy();
+    });
+
+    document.addEventListener("shopify:block:select", function (event) {
+      var block = JSON.parse(event.target.dataset.shopifyEditorBlock);
+      if (block.type === "banner_slide") {
+        console.log("Swiper block is", event.target.dataset.slideIndex)
+        swiper.slideTo(+event.target.dataset.slideIndex);
+      }
+    });
+  }
 });
