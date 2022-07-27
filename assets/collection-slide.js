867a4d285decbaabf1e0ba6714352ee285101140
KevinMwita7 <kevinmwita7@gmail.com>
2022-07-27 22:14:39 +0000

    use strict comparison

--- assets/collection-slide.js
+++ assets/collection-slide.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   let swiperHtmlEl = document.getElementById("collection-list-swiper");
   let enableSwipeOnMobile = swiperHtmlEl.getAttribute("data-swipe-on-mobile");
-
+  console.log(enableSwipeOnMobile);
   let swiper = new Swiper('.collection-list-swiper', {
-    allowSlideNext: enableSwipeOnMobile == "false" ? false : true,
-    allowSlidePrev: enableSwipeOnMobile == "false" ? false : true,
+    allowSlideNext: enableSwipeOnMobile === "false" ? false : true,
+    allowSlidePrev: enableSwipeOnMobile === "false" ? false : true,
     slidesPerView: +swiperHtmlEl.getAttribute("data-slides-per-view"),
     spaceBetween: 30,
     // If we need pagination
